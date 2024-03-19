3d8ba90244e8d9ff44386b57e874f53659bdb150
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-20 00:44:17 +0700

    feat: add language icons to footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,3 +1,31 @@
+import Image from "next/image"
+
+import { Button } from "@/components/ui/button"
+
 export default function Footer() {
-  return <div>Footer</div>
+  const languages = [
+    { name: "Croatian", icon: "/hr.svg" },
+    { name: "Spanish", icon: "/es.svg" },
+    { name: "French", icon: "/fr.svg" },
+    { name: "Italian", icon: "/it.svg" },
+    { name: "Japanese", icon: "/jp.svg" },
+  ]
+  return (
+    <footer className="hidden h-20 w-full border-t-2 border-slate-200 p-2 lg:block">
+      <div className="mx-auto flex h-full max-w-screen-lg items-center justify-evenly">
+        {languages.map((language) => (
+          <Button key={language.name} size="lg" variant="ghost">
+            <Image
+              src={language.icon}
+              alt={language.name}
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            {language.name}
+          </Button>
+        ))}
+      </div>
+    </footer>
+  )
 }
