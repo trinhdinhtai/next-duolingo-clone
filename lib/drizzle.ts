328293efb7bdea6fa8a57734f15680bf96f8217e
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-23 00:22:26 +0700

    feat: add drizzle and schema

---
+++ lib/drizzle.ts
@@ -0,0 +1,12 @@
+import { neon } from "@neondatabase/serverless"
+import { drizzle } from "drizzle-orm/neon-http"
+
+import { env } from "@/env"
+
+import * as schema from "./schema"
+
+const sql = neon(env.DATABASE_URL)
+// @ts-ignore
+const db = drizzle(sql, { schema })
+
+export default db
